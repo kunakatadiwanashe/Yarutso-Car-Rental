9826fcfd43ce16316474763bb16e2d1b9f15d092
kunakatadiwanashe <kunakatadiwanshe99@gmail.com>
2022-05-17 10:50:58 +0200

    banner

---
+++ booking.php
@@ -0,0 +1,98 @@
+
+
+
+
+<div id="booking" class="section">
+    <div class="section-center">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-5">
+                    <div class="booking-form">
+                        <form>
+                            <div class="form-group">
+                                <span class="form-label">pick-up location (city, state or Airport)</span>
+                                <input class="form-control" type="text" placeholder="Enter a destination or hotel name">
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">pick up date</span>
+                                        <input class="form-control" type="date" required>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">return date</span>
+                                        <input class="form-control" type="date" required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">pick-up time</span>
+                                        <input class="form-control" type="time" required>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">return time</span>
+                                        <input class="form-control" type="time" required>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">driver's email</span>
+                                        <input class="form-control" type="email" required>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">driver's tel</span>
+                                        <input class="form-control" type="tel" required>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                          
+
+
+                            <div class="form-btn">
+                                <button class="submit-btn">Check availability</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="col-md-7">
+                    <div class="booking-cta">
+                        <h1>Make your reservation</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facere, soluta magnam consectetur molestias itaque
+                            ad sint fugit architecto incidunt iste culpa perspiciatis possimus voluptates aliquid consequuntur cumque quasi.
+                            Perspiciatis.
+                        </p>
+                        <h3>Harare office</h3>
+                        <p class="cul">Address <span></span> 797 Grosvenor,  Highlands</p>
+                        <p class="cul">Email<span></span> yarutsocarhire@gmail.com</p>
+                        <p class="cul">Mobile <span></span> 0782 255 216</p>
+                        
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
